8c8739a09c22ebba946ea0767a173e5a83f149b4
Efraim Nabil <efraimefefraim@gmail.com>
2024-02-26 04:04:02 +0200

    Add pagination and sorting functionality to TodoList component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -7,6 +7,7 @@ import Modal from "./ui/Modal";
 import Textarea from "./ui/Textarea";
 import axiosInstance from "../config/axios.config";
 import TodoSkeleton from "./TodoSkeleton";
+import Paginator from "./Paginator";
 const TodoList = () => {
   const storageKey = "loggedInUser";
   const userDataString = localStorage.getItem(storageKey);
@@ -16,6 +17,9 @@ const TodoList = () => {
   const [isOpenConfirmModal, setIsOpenConfirmModal] = useState(false);
   const [isOpenAddModal, setIsOpenAddModal] = useState(false);
   const [queryVersion, setQueryVersion] = useState(1);
+  const [page, setPage] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(10);
+  const [sortBy, setSortBy] = useState<'DESC' | "ASC">("DESC");
   const [editTodo, setEditTodo] = useState<ITodo>({
     id: 0,
     title: "",
@@ -26,18 +30,36 @@ const TodoList = () => {
     title: "",
     description: "",
   })
-  const {isLoading, data} = useAuthQuery({
+  const {isLoading, data, isFetching} = useAuthQuery({
     // todoList+$todotoedit.id
-    queryKey: ["todoList", `${queryVersion}`],
-    url: "/users/me?populate=todos",
+    queryKey: [`todos-page-${page}`, `${pageSize}`, `${queryVersion}`, `${sortBy}`],
+    url: `/todos?pagination[pageSize]=${pageSize}&pagination[page]=${page}&sort=createdAt:${sortBy}`,
     config: {
       headers: {
         Authorization: `Bearer ${userData?.jwt}`,
       },
     },
   });
+  console.log(data);
 
   // ** Handlers
+
+  const onClickPrev = () => {
+    setPage((prev) => prev - 1);
+  }
+
+  const onClickNext = () => {
+    setPage((prev) => prev + 1);
+  }
+
+  const onChangePageSize = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setPageSize(+e.target.value);
+  }
+
+  const onChangeSortBy = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSortBy(e.target.value as "DESC" | "ASC");
+  }
+
   const onCloseEditModal = () => {
     setEditTodo({
       id: 0,
@@ -149,21 +171,15 @@ const TodoList = () => {
     }
   }
   
-
-  if(isLoading) return (
-    <div className="space-y-1">
-      {Array.from({ length: 3 }).map((_, i) => <TodoSkeleton key={i} />)}
-    </div>
-  )
-
-  const renderTodos = data.todos.map((todo: ITodo) => (
-    <div key={todo.id} className="flex items-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md even:bg-gray-100">
-      <p className="w-full font-semibold">{1 + ". " + todo.title}</p>
+    
+  const renderTodos = data?.data?.map(({id, attributes } : {id: number, attributes: ITodo}, i: number) => (
+    <div key={id} className="flex items-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md even:bg-gray-100">
+      <p className="w-full font-semibold">{id + " " + i + ". " + attributes.title}</p>
       <div className="flex items-center justify-end w-full space-x-3">
-        <Button size={"sm"} onClick={() => onOpenEditModal(todo)} isLoading={isUpdating}>
+        <Button size={"sm"} onClick={() => onOpenEditModal({id, title: attributes.title, description: attributes.description})} isLoading={isUpdating}>
           Edit
         </Button>
-        <Button variant={"danger"} size={"sm"} onClick={() => openConfirmModal(todo)}>
+        <Button variant={"danger"} size={"sm"} onClick={() => openConfirmModal({id, title: attributes.title, description: attributes.description})}>
           Remove
         </Button>
       </div>
@@ -172,18 +188,47 @@ const TodoList = () => {
 
   return (
     <div className="space-y-1 ">
-      <div className="w-fit mx-auto my-10">
-        <Button onClick={openAddModal} className="bg-indigo-700 hover:bg-indigo-800">
+      <div className="flex items-center justify-between space-x-2">
+        <Button size={"sm"} onClick={openAddModal}>
           Add Todo
         </Button>
-      </div>
-      
+        <div className="flex items-center justify-between space-x-2 text-md">
+          <select className="border-2 border-indigo-600 rounded-md p-2" value={sortBy} onChange={onChangeSortBy}>
+            <option disabled>Sort by</option>
+            <option value="ASC">Oldest</option>
+            <option value="DESC">Latest</option>
+          </select>
 
-       { data.todos.length? (
-          renderTodos
-       ) : (
-         <p>No todos found</p>
-       )}
+          <select className="border-2 border-indigo-600 rounded-md p-2" value={pageSize} onChange={onChangePageSize}>
+            <option disabled>Page size</option>
+            <option value={10}>10</option>
+            <option value={50}>50</option>
+            <option value={100}>100</option>
+          </select>
+        </div>
+      </div>
+
+       {
+        isLoading ? (
+          <div className="space-y-1">
+            {Array.from({ length: pageSize }).map((_, i) => <TodoSkeleton key={i} />)}
+          </div>
+        ) : ( 
+              data.data?.length? (
+                  renderTodos
+              ) : (
+                <p>No todos found</p>
+              )
+            )
+          }
+        <Paginator 
+          page={page}
+          pageCount={data?.meta?.pagination.pageCount}
+          total={data?.meta?.pagination.total}
+          isLoading={isLoading || isFetching}
+          onClickPrev={onClickPrev}
+          onClickNext={onClickNext}
+        />
 
        {/* Add todo Modal */}
        <Modal isOpen={isOpenAddModal} closeModal={closeAddModal} title="Add Todo">
