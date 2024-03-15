6c655c7d48405b9d6df0085fcb294cc3f3e43a2e
Efraim Nabil <efraimefefraim@gmail.com>
2024-03-16 01:02:26 +0200

    Update Select component styling

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -13,7 +13,7 @@ const Select = ({ selected, setSelected, options }: IProps) => {
     return (
         <Listbox value={selected} onChange={setSelected}>
             <div className="relative mt-1">
-                <Listbox.Button className="relative w-full cursor-default rounded-lg text-white bg-gradient-to-bl from-pink-trans to-orange-trans py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                <Listbox.Button className="relative w-[148px] cursor-default rounded-lg text-white bg-gradient-to-bl from-pink-trans to-orange-trans py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
                     <span className="block truncate">{selected.name}</span>
                     <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                         <ChevronUpDownIcon
