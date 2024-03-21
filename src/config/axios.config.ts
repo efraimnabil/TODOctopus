740044c27c0ab211179c8b037996630afdc3735e
Efraim Nabil <efraimefefraim@gmail.com>
2024-03-21 23:23:56 +0200

    Update axios base URL

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://todo-app-pz34.onrender.com/api/v1',
+  baseURL: 'https://todoctopus-ruxs.onrender.com/api/v1',
 });
 
 export default axiosInstance;
